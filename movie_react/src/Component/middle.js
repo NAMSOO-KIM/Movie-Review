acf3d98acc04d0209566a0b2046d4c81a4be3858
hangyujung <ssikssikii5@gmail.com>
2020-04-17 22:56:50 +0900

    0417gyu

---
+++ movie_react/src/Component/middle.js
@@ -0,0 +1,16 @@
+import React,{Component} from 'react';
+import styled from 'styled-components';
+import './middle.css'
+
+class middle extends Component{
+    render(){
+    return (
+        <div>
+        <h1>분석된 영화 리뷰를 확인하세요</h1>
+        <h3>인공지능 분석을 통한 영화 리뷰</h3> 
+        </div>            
+  );
+}
+
+}
+export default middle;
---
+++ movie_react/src/Component/top.js
@@ -0,0 +1,17 @@
+import React,{Component} from 'react';
+import styled from 'styled-components';
+import logo from '../img/HomeButton.PNG'
+
+class top extends Component{
+    render(){
+    return (
+        
+                    <img 
+                    src={logo} alt="홈버튼"/>
+        
+   
+  );
+}
+
+}
+export default top;
